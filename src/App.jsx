98bc0fb88d905df08566f293db4bc8bf3c3c03c2
Jana Conjo <JanaConjoo@gmail.com>
2025-08-21 12:16:52 +0200

    v32

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Inicio from "./components/Inicio";
+import Inicio from "./components/inicio";
 import ReservarMesa from "./components/reservarmesa";
 
 export default function App() {
